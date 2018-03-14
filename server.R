c9f3f6dcb89d6c5f35810658c7226c0d462be593
reiqu <pawevv@gmail.com>
2018-03-14 22:24:38 +0100

    Dodane Widgety do UI, dodana logika do niewidzialnych widgetów. Poprawiony wygląd UI. Drobne zmiany w server.R

--- server.R
+++ server.R
@@ -12,6 +12,8 @@ wyliczBmr <-function(waga,wzrost,wiek,plec)
 
 server <- function(input, output)
 {
+  shinyjs::hide("trening")
+  shinyjs::hide("inten")
   options(encoding = 'UTF-8')
 	options(digits = 5)
   
@@ -31,11 +33,31 @@ server <- function(input, output)
       stringsAsFactors=FALSE)
     
   })
-  
-  output$l <- renderTable({
+  typ_zwracany <- reactive({typ <- switch(input$typ,
+        ekto = 800,
+        mezomorf = 450,
+        endo = 300)})
+  czy_cwiczy <- reactive({typ <- switch(input$czycwiczy,
+        Tak = 1,
+        Nie = 0)})
+
+  observe({
+    cwiczy <- czy_cwiczy()
+    if(cwiczy == 1){
+      shinyjs::show("trening",anim = TRUE, animType ="slide",time =0.5)
+      shinyjs::show("inten",anim = TRUE, animType ="slide",time =0.5)
+    }
+      else{
+      shinyjs::hide("trening",anim = TRUE, animType ="slide",time =0.5)
+      shinyjs::hide("inten",anim = TRUE, animType ="slide",time =0.5)
+    }
+    typ <- typ_zwracany()
+    observeEvent(input$oblicz,{output$m <-renderText({wyliczBmr(isolate(value$waga),isolate(value$wzrost),isolate(value$wiek),isolate(value$plec)) + typ})})
+    output$l <- renderTable({
     sliderValues()
   })
-  observeEvent(input$oblicz,{output$m <-renderText({wyliczBmr(isolate(value$waga),isolate(value$wzrost),isolate(value$wiek),isolate(value$plec))})})
+    })
+  
   
   
 }
--- ui.R
+++ ui.R
@@ -2,40 +2,49 @@ library(shiny)
 library(shinyjs)
 
 ui <- fluidPage(
-	useShinyjs(),
+	
 	headerPanel(title = "Kalkulator kalorii"),
 	#Main panel for displaying outputs
    sidebarLayout(
-      sidebarPanel(
+      div(style="width: 100%",sidebarPanel(
 	helpText("Wylicz łatwo zapotrzebowanie kaloryczne ale najpierw uzupełnij dane"),
 	h6(helpText("Pamietaj, że dane podane są z przybliżeniem")),
-	selectInput("plec",
+	div(style="display: inline-block;vertical-align:top; width: 150px;",selectInput("plec",
 		label = "Wybierz płeć",
 		choices = c("Mężczyzna" = "M",
 				"Kobieta" = "K"),
-		selected = "Mężczyzna"),
-	numericInput("wiek", label = "Podaj wiek",
-		20,min = 5, max = 100),
-	numericInput("waga",
-		label = "Podaj wage",
-		70, min = 1, max = 200),
-	numericInput("wzrost",
-		label = "Wzrost",180,
-		min = 130, max =210),
+		selected = "Mężczyzna")),
+	div(style="display: inline-block;vertical-align:top; width: 150px;",numericInput("wiek", label = "Podaj wiek",20,min = 5, max = 100)),
+	div(style="display: inline-block;vertical-align:top; width: 150px;",numericInput("waga",label = "Podaj wage",70, min = 1, max = 200)),
+	div(style="display: inline-block;vertical-align:top; width: 150px;",numericInput("wzrost",label = "Wzrost",180,min = 130, max =210)),
+	hr(),
 	h6(helpText("Zaznacz 'Tak' jeśli chcesz podać szczegóły treningu a algorytm wykona bardziej precyzyjne obliczenia")),
-	selectInput("czycwiczy",
+	div(style="display: inline-block;vertical-align:top; width: 150px;",selectInput("czycwiczy",
 		label = "Uprawiasz jakiś sport ?",
 		choices =c("Tak",
 				"Nie"),
-		selected = "Nie"),
-	actionButton("oblicz","Oblicz")
+		selected = "Nie")),
+	div(style="display: inline-block;vertical-align:top; width: 75px;"),
+	div(style="display: inline-block;vertical-align:top; width: 150px;",radioButtons("typ","Typ budowy ciała:",
+		c("Ektomorfik" = "ekto",
+			"Mezomorfik" = "mezomorf",
+			"Endomorfik" = "endo"))),
+	hr(),
+	div(style="display: inline-block; vertical-align:top; width: 150px;",radioButtons("trening","Typ treningu:",
+		c("Siłowy" = "sila",
+			"Aerobowy" = "aero"))),
+	div(style="display: inline-block; vertical-align:top; width: 150px;", sliderInput("inten","Intesywność treningu: ",min =7,max=9,value =8,step=1)),
+	hr(),
+	div(style="display: inline-block; vertical-align:top; width: 150px;",actionButton("oblicz","Oblicz"))
 	
 	
 	
-	
+	)
 	),
-	mainPanel(textOutput("m"),
-	          tableOutput("l")
-	           )
-)
+	mainPanel(
+		useShinyjs(),
+		textOutput("m"),
+	   	tableOutput("l")
+	    )
+	)
 )
